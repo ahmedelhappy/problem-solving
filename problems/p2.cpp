0366794739c05f628118e7ed9893f129cef0ebe3
ahmedElhappy <ahmedelhappy@gmail.com>
2025-02-15 03:12:25 +0200

    add another 3 for later ot make it ez

---
+++ problems/p2.cpp
@@ -0,0 +1,45 @@
+// Problem #2: Sort 3 numbers
+// ● Given 3 integers, sort (order) them in ascending order and print them .
+// ● Inputs ⇒ outputs
+// ○ 1 2 3 ⇒ 1 2 3
+// ○ 1 3 2 ⇒ 1 2 3
+// ○ 2 1 3 ⇒ 1 2 3
+// ○ 2 3 1 ⇒ 1 2 3
+// ○ 3 1 2 ⇒ 1 2 3
+// ○ 3 2 1 ⇒ 1 2 3
+// ● Do you notice there are only 6 ways to permutate 3 numbers!
+
+
+// Problem #3: Maximum but constrained
+// ● Given 3 integers, you have to find the biggest one of them which is < 100.
+// ○ Print -1 if no such number
+// ● Inputs
+// ○ 22 90 115 ⇒ 90
+// ■ Here [20 90] are only < 100. Maximum (20, 90) = 90
+// ○ 200 300 400 ⇒ -1
+// ■ All of them are > 100, so no answer
+// ○ 50 100 150 ⇒ 50
+// ■ Only 50 is < 100.
+// ○ 10 30 20 ⇒ 30
+// ■ The 3 numbers < 100, so their max is 30
+
+
+// Problem #4: Conditional Count
+// ● Write a program that reads number X, then other 5 numbers. Print 2 values:
+// ○ How many numbers <= X
+// ○ How many numbers > X
+// ○ Any relation between these 2 outputs?
+// ● Inputs
+// ○ 10 300 1 5 100 200
+// ○ Output: 2 3
+// ○ Explantation
+// ○ 2 numbers (1, 5) are <= 10
+// ○ 3 numbers (100, 200, 300) are > 10
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+  
+}
