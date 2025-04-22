927d91b7774ca49625e3db4ba377a18f6842bdff
ahmedElhappy <ahmedelhappy@gmail.com>
2025-04-22 09:12:52 +0200

    Solve Problem4

--- problems/p4.cpp
+++ problems/p4.cpp
@@ -9,3 +9,45 @@
 // ○ Explanation
 // ○ 2 numbers (1, 5) are <= 10
 // ○ 3 numbers (100, 200, 300) are > 10
+
+
+
+//My First Solution
+// #include <iostream>
+// using namespace std;
+
+// int main () {
+//   int x, num1, num2, num3, num4, num5;
+//   cin >> x >> num1 >> num2 >> num3 >> num4 >> num5;
+//   int lessCounter;
+//   int greaterCounter = 0;
+//   if (x < num1) greaterCounter++;
+//   if (x < num2) greaterCounter++;
+//   if (x < num3) greaterCounter++;
+//   if (x < num4) greaterCounter++;
+//   if (x < num5) greaterCounter++;
+//   lessCounter = 5 - greaterCounter;
+//   cout << lessCounter << " " << greaterCounter;
+// }
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int x, a1, a2, a3, a4, a5;
+
+	cin >> x >> a1 >> a2 >> a3 >> a4 >> a5;
+
+	int cnt = 0;
+
+	cnt += (a1 <= x);
+	cnt += (a2 <= x);
+	cnt += (a3 <= x);
+	cnt += (a4 <= x);
+	cnt += (a5 <= x);
+
+	cout << cnt << " " << 5 - cnt << "\n";
+
+	return 0;
+}
