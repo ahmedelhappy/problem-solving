01ef41a8c39a6f2d9523dc277d9d450637d012be
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-28 07:23:09 +0300

    Perfect Diamond Made the code cleaner, Added functions for stars and spaces. Added The middle row of stars to be perfect Diamond.

--- Done/P07p2n1.cpp
+++ Done/P07p2n1.cpp
@@ -1,51 +1,51 @@
 #include <iostream>
 using namespace std;
 
+void printSpaces(int N, int line)
+{
+  int numOfSpaces = N - line + 1;
+  while (numOfSpaces > 0)
+  {
+    cout << " ";
+    numOfSpaces--;
+  }
+}
+void printStars(int line)
+{
+  int numOfStars = (line * 2) - 1;
+  while (numOfStars > 0)
+  {
+    cout << "*";
+    numOfStars--;
+  }
+}
+
+
+
+
+
 int main()
 {
   int N;
   cin >> N;
-
-  // int numOfStars, numOfSpaces;
+  //==============Upper Triangle============
   int line = 1;
-
   while (line <= N)
   {
-    int numOfSpaces = N - line;
-    while (numOfSpaces > 0)
-    {
-      cout << " ";
-      numOfSpaces--;
-    }
-
-    int numOfStars = (line * 2) - 1;
-    while (numOfStars > 0)
-    {
-      cout << "*";
-      numOfStars--;
-    }
-
+    printSpaces(N, line);
+    printStars(line);
     cout << "\n";
     line++;
   }
-//===============Reverse==================
+  //===============Middle Row=================
+  printStars(N + 1);
+  cout << "\n";
+  //============Lower Triangle==============
   line = N;
   while (line >= 1)
   {
-    int numOfSpaces = N - line;
-    while (numOfSpaces > 0)
-    {
-      cout << " ";
-      numOfSpaces--;
-    }
-
-    int numOfStars = (line * 2) - 1;
-    while (numOfStars > 0)
-    {
-      cout << "*";
-      numOfStars--;
-    }
-
+    printSpaces(N, line);
+    printStars(line);
     cout << "\n";
     line--;
   }
